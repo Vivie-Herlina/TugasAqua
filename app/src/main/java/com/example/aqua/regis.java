8471275204365fc1eb741bcc99b96719dc56a541
Abstract Programmer <AbstractProgrammer@example.com>
2024-04-24 21:18:00 +0700

    first commit

---
+++ app/src/main/java/com/example/aqua/regis.java
@@ -0,0 +1,27 @@
+package com.example.aqua;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class regis extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_regis);
+
+    }
+
+    public void back(View v) {
+        Intent intent = new Intent (regis.this, loginregis.class);
+        startActivity(intent);
+    }
+}
